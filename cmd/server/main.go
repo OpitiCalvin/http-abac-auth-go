c06048e25fb0300801542d51436b50a37f061eb0
Opiti Calvin <opiticalvin@gmail.com>
2021-06-06 19:12:08 +0200

    http server initialization/execution

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,79 +1,69 @@
 package main
 
 import (
-	"fmt"
-
-	// "log"
-	// "novelsTradeIn/pkg/api"
-	// "novelsTradeIn/pkg/repository"
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
 	"os"
+	"time"
 
-	"github.com/OpitiCalvin/http-abac-auth-go/pkg/server"
+	"github.com/OpitiCalvin/http-abac-auth-go/pkg/api/middleware"
+	"github.com/OpitiCalvin/http-abac-auth-go/pkg/usecase/partner"
+
+	"github.com/OpitiCalvin/http-abac-auth-go/pkg/api/handler"
+	"github.com/OpitiCalvin/http-abac-auth-go/pkg/repository"
+	"github.com/urfave/negroni"
+
 	"github.com/gorilla/mux"
-	// "github.com/joho/godotenv"
-	// "gorm.io/gorm"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	if err := run(); err != nil {
-		fmt.Println(os.Stderr, "this is the startup error: %s\n", err)
-		os.Exit(1)
-	}
-}
-
-// run function is responsible for setting up db connections, routers etc
-func run() error {
-	// // load env params
-	// err := godotenv.Load()
-	// if err != nil {
-	// 	log.Fatal(err) // error loading env file
-	// }
-
-	// username := os.Getenv("USERNAME")
-	// password := os.Getenv("PASSWORD")
-	// dbname := os.Getenv("DBNAME")
-	// host := os.Getenv("HOST")
-	// DBPort := os.Getenv("DBPORT")
-
-	// connectionString := fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable", username, password, host, DBPort, dbname)
-
-	// // setup a connection to database
-	// db, err := setupDatabase(connectionString)
-	// if err != nil {
-	// 	return err
-	// }
-
-	// // create storage dependency
-	// storage := repository.NewStorage(db)
-
-	// // run migrations
-	// err = storage.RunMigrations(connectionString)
-	// if err != nil {
-	// 	return err
-	// }
-
-	// create router dependency
-	router := mux.NewRouter()
-	// TO DO: implement CORS with router
-
-	// // create user service
-	// userService := api.NewUserService(storage)
-
-	// start server
-	server := server.NewServer(router)
-	err := server.Run()
+	db, err := sql.Open("sqlite3", "./test.db")
 	if err != nil {
-		return err
+		log.Fatal(err.Error())
 	}
 
-	return nil
+	defer db.Close()
+
+	partnerRepo := repository.NewPartnerDB(db)
+	partnerService := partner.NewService(partnerRepo)
+
+	r := mux.NewRouter()
+	// handlers
+	n := negroni.New(
+		negroni.HandlerFunc(middleware.Cors),
+		negroni.NewLogger(),
+	)
+
+	// partner
+	handler.MakePartnerHandlers(r, *n, partnerService)
+
+	http.Handle("/", r)
+	r.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		response := map[string]interface{}{
+			"status": "success",
+			"data":   "API running smoothly",
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(response)
+	})
+
+	logger := log.New(os.Stderr, "logger: ", log.Lshortfile)
+	srv := &http.Server{
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		// Addr:         ":" + strconv.Itoa(config.API_PORT),
+		Addr: ":8080",
+		// Handler:      context.ClearHandler(http.DefaultServeMux),
+		Handler:  http.DefaultServeMux,
+		ErrorLog: logger,
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
-
-// func setupDatabase(connString string) (*gorm.DB, error) {
-// 	db, err := gorm.Open("postgres", connString)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	return db, nil
-// }
