e5626ad45a96acd39e54fb3e20b02783cdf3075b
Opiti Calvin <opiticalvin@gmail.com>
2021-06-06 19:08:31 +0200

    old/initial  server files removed

--- pkg/server/routes.go
+++
@@ -1,8 +0,0 @@
-package server
-
-func (s *Server) InitializeRoutes() {
-	router := s.Router
-
-	router.HandleFunc("/status", s.ApiStatus()).Methods("GET")
-	router.HandleFunc("/", s.IndexHandler()).Methods("GET")
-}
