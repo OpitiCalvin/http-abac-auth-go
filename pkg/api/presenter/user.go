2cf26db64ca6c3cd6d4ecf1b6e8746f3ce542630
Opiti Calvin <opiticalvin@gmail.com>
2021-06-10 19:57:21 +0200

    all presenter structs placed in a single file

--- pkg/api/presenter/user.go
+++
@@ -1,9 +0,0 @@
-package presenter
-
-// User data
-type User struct {
-	ID       int    `json:"id"`
-	Email    string `json:"email"`
-	Username string `json:"username"`
-	// Client    Client    `json:"client"`
-}
