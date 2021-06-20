91061a0de7949a361ca80e9cd78b716109de8b8e
Opiti Calvin <opiticalvin@gmail.com>
2021-06-20 18:45:29 +0200

    user login capability and generation of JWTs implemented

--- pkg/usecase/user/service.go
+++ pkg/usecase/user/service.go
@@ -2,12 +2,25 @@ package user
 
 import (
 	"errors"
+	"os"
 	"time"
+
+	"github.com/OpitiCalvin/http-abac-auth-go/pkg/utils"
 
 	"github.com/OpitiCalvin/http-abac-auth-go/pkg/entity"
 
 	"github.com/OpitiCalvin/http-abac-auth-go/pkg/usecase/client"
+
+	jwt "github.com/dgrijalva/jwt-go"
 )
+
+/*
+ *	JWT claims struct
+ */
+type Token struct {
+	UserId int64
+	jwt.StandardClaims
+}
 
 // Service create user usecase service
 type UserService struct {
@@ -88,4 +101,30 @@ func (s *UserService) DeleteUser(id int64) error {
 	}
 
 	return s.repo.Delete(id)
+}
+
+// LoginUser authenticates a user and generates a jwt token
+func (s *UserService) LoginUser(usernameOrEmail, password string) map[string]interface{} {
+	user, err := s.repo.FindByUsername(usernameOrEmail)
+	if err != nil {
+		user, err = s.repo.FindByEmail(usernameOrEmail)
+		if err != nil {
+			return utils.Message(false, "Invalid login credentials")
+		}
+	}
+
+	if passValid := entity.CheckPassword(user.Password, password); !passValid {
+		return utils.Message(false, "Invalid login credentials")
+	}
+
+	// Create JWT token
+	tk := &Token{UserId: user.ID}
+	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), tk)
+	tokenString, _ := token.SignedString([]byte(os.Getenv("TOKENKEY")))
+
+	resp := utils.Message(true, "Logged In")
+	resp["user"] = user
+	resp["token"] = tokenString
+
+	return resp
 }
